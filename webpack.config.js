2a80ce1de5795ed40fe054a3e75ae832017b6f55
Miguel Palau <mpalauzarza@gmail.com>
2016-10-22 12:46:16 -0500

    made a switch for the choices

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
 // To merge 2 config files together!
 const merge = require('webpack-merge')
 
-// Thing to check what's running on `npm start` or build, etc
-const TARGET = process.env.npm_lifecycle_event
 const start = require('./webpack/webpack.start')
 const production = require('./webpack/webpack.production')
 const common = require('./webpack/webpack.common')
 
-// Use only with start command
-if ( TARGET === 'start' || !TARGET ) {
-  module.exports = merge(common, start)
-}
-
-if ( TARGET === 'build' || !TARGET ) {
-  module.exports = merge(common, {})
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    module.exports = merge(common, production)
+    break;
+  default:
+    module.exports = merge(common, start)
 }
