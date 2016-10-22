758f99ca600775ed23e5a0c3c2bbe93296c20dc5
Miguel Palau <mpalauzarza@gmail.com>
2016-10-22 12:35:48 -0500

    made paths file better with path

--- webpack.paths.js
+++ webpack.paths.js
@@ -4,8 +4,8 @@ const path = require('path');
 const PATHS = {
   app: path.join( __dirname, 'src' ),
   build: path.join( __dirname, 'dist' ),
-  start: path.join( __dirname, 'src' ) + '/index.html',
-  styles: path.join( __dirname, 'src' ) + '/styles/1-abstracts'
+  start: path.join( __dirname, 'src', 'index.html' ),
+  styles: path.join( __dirname, 'src', 'styles', '1-abstracts' )
 }
 
 module.exports = PATHS
